f82e71d49c20680ed97f1919637496f48d697e63
ryzeiron <151409242+ryzeiron@users.noreply.github.com>
2026-04-21 21:45:01 +0200

    Update catalog.ts

--- lib/catalog.ts
+++ lib/catalog.ts
@@ -5,7 +5,10 @@
 export type Condition = "Mint" | "Near Mint" | "Excellent" | "Good" | "Played";
 export type Rarity =
   | "Reverse"
+  | "Reverse"
   | "Holo"
+  | "Holo Cracked Ice"
+  | "Holo ligne"
   | "Rare Reverse"
   | "Rare Holo"
   | "Ultra Rare"
