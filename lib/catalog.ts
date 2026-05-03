b3254cd193dcf1caa4db158e921e9af9f78c4799
ryzeiron <151409242+ryzeiron@users.noreply.github.com>
2026-05-03 16:10:56 +0200

    Update catalog.ts

--- lib/catalog.ts
+++ lib/catalog.ts
@@ -197,7 +197,7 @@ export const SERIES: Serie[] = [
   { id: "me02.5", blocId: "mega-evolution", code: "ME02.5", name: "Heros Transcendants", releaseYear: 2026,},
   { id: "me03", blocId: "mega-evolution", code: "ME03", name: "Equilibre Parfait", releaseYear: 2026,},
   { id: "me04", blocId: "mega-evolution", code: "ME04", name: "Chaos Ascendant", releaseYear: 2026,},
-  { id: "me05", blocId: "mega-evolution", code: "ME05", name: "Nuit Noire", releaseYear: 2026, image: "/series/ME/me05.png",, comingSoon: true },
+  { id: "me05", blocId: "mega-evolution", code: "ME05", name: "Nuit Noire", releaseYear: 2026, image: "/series/ME/me05.png", comingSoon: true },
 
   // Ecarlate et Violet
   { id: "promo", blocId: "ecarlate-et-violet", code: "Promo", name: "Ecarlate et Violet", releaseYear: 2023, image: "/series/EV/SVP.webp", },
