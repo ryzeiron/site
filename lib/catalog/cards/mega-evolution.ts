a76cd26fe07850babe46353b50e84e03b07ee92f
ryzeiron <151409242+ryzeiron@users.noreply.github.com>
2026-05-05 21:15:56 +0200

    Update mega-evolution.ts

--- lib/catalog/cards/mega-evolution.ts
+++ lib/catalog/cards/mega-evolution.ts
@@ -1,1 +1,6 @@
+import type { Card } from "../../catalog";
 
+// epee-et-bouclier - 2966 cards
+export const MEGA_EVOLUTION_CARDS = ([
+{ id: "mega-evolution-001", serieId: "me-promo", name: "Méganium", number: "001", rarity: "Reverse", condition: "Near Mint", language: "FR", price: 0.5, stock: 0, image: "/cartes/mep/1.webp", },
+] as unknown as Card[]);
