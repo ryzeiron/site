cc08bf0ad4c599e80b0d737ab75b975e6b32e053
ryzeiron <151409242+ryzeiron@users.noreply.github.com>
2026-05-06 23:31:46 +0200

    Create heartgold-soulsilver.ts

---
+++ lib/catalog/cards/heartgold-soulsilver.ts
@@ -0,0 +1,6 @@
+import type { Card } from "../../catalog";
+
+export const HEARTGOLD_SOULSILVER_CARDS = [
+  { id: "prhgss-001", serieId: "prhgss", name: "Ho-Oh", number: "HGSS01", rarity: "Promo", condition: "Near Mint", language: "FR", price: 1, stock: 0, image: "/cartes/prhgss/1.webp", },
+
+] as unknown as Card[];
