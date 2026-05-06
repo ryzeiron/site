b88bef69fb69029d214cd952fa93ba4241dab265
ryzeiron <151409242+ryzeiron@users.noreply.github.com>
2026-05-06 23:26:49 +0200

    Update catalog.ts

--- lib/catalog.ts
+++ lib/catalog.ts
@@ -4,7 +4,7 @@ import { EPEE_ET_BOUCLIER_CARDS } from "./catalog/cards/epee-et-bouclier";
 import { SOLEIL_ET_LUNE_CARDS } from "./catalog/cards/soleil-et-lune";
 import { XY_CARDS } from "./catalog/cards/xy";
 import { NOIR_ET_BLANC_CARDS } from "./catalog/cards/noir-et-blanc";
-import { HEARTGOLD_SOULSILVER_CARDS } from "./catalog/cards/noir-et-blanc";
+import { HEARTGOLD_SOULSILVER_CARDS } from "./catalog/cards/heartgold-soulsilver";
 
 // Catalogue : blocs > series > cartes
 // Prix en euros (ex: 15 = 15 EUR, 0.5 = 50 centimes)
