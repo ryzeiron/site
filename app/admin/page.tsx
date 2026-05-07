d5fe1d56ebe306ef2592a1ecbf600d31cbad2e29
ryzeiron <151409242+ryzeiron@users.noreply.github.com>
2026-05-07 18:15:06 +0200

    Update page.tsx

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+import Link from "next/link"; 
 import { redirect } from "next/navigation";
 import AdminStockRow from "@/components/AdminStockRow";
 import LogoutButton from "@/components/LogoutButton";
