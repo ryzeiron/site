eeff97ea8701fd9c7a9a9bb5ecba5eb23bd4374c
ryzeiron <151409242+ryzeiron@users.noreply.github.com>
2026-05-08 17:17:17 +0200

    Update schema.ts

--- lib/db/schema.ts
+++ lib/db/schema.ts
@@ -1,4 +1,4 @@
-import { integer, pgTable, primaryKey, text, timestamp } from "drizzle-orm/pg-core";
+import { integer, pgTable, primaryKey, text, timestamp } from "drizzle-orm/pg-core"; 
 
 export const stockOverrides = pgTable(
   "stock_overrides",
