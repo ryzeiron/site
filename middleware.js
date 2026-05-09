9185f535dada1460c66f5451dee266e3fc547fcc
ryzeiron <151409242+ryzeiron@users.noreply.github.com>
2026-05-09 15:44:54 +0200

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -4,7 +4,7 @@ export function middleware(req) {
   const authHeader = req.headers.get('authorization')
  
   const USER = 'admin' 
-  const PASS = 'Poupoune15@'
+  const PASS = 'Poupoune15@' 
 
   const expected = 'Basic ' + btoa(`${USER}:${PASS}`)
 
