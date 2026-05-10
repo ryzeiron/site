4d894b37f5c58b7ccbd5b1b997826f14a5a430cf
ryzeiron <151409242+ryzeiron@users.noreply.github.com>
2026-05-10 21:45:18 +0200

    Update SplashIntro.tsx

--- components/SplashIntro.tsx
+++ components/SplashIntro.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 
-const SPLASH_LOGO_SRC = "/logo.jpg";
+const SPLASH_LOGO_SRC = "/logo.png";
 const SPLASH_DURATION_MS = 1600;
 const SPLASH_FADE_DELAY_MS = 1100;
 
