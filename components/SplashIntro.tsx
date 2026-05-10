c53ec1efd637f3d23b55b22e85abe57918ed0a6d
ryzeiron <151409242+ryzeiron@users.noreply.github.com>
2026-05-10 18:56:30 +0200

    Update SplashIntro.tsx

--- components/SplashIntro.tsx
+++ components/SplashIntro.tsx
@@ -3,8 +3,8 @@
 import { useEffect, useState } from "react";
 
 const SPLASH_LOGO_SRC = "/logo.png";
-const SPLASH_DURATION_MS = 2600;
-const SPLASH_FADE_DELAY_MS = 2100;
+const SPLASH_DURATION_MS = 1600;
+const SPLASH_FADE_DELAY_MS = 1100;
 
 export default function SplashIntro() {
   const [visible, setVisible] = useState(true);
