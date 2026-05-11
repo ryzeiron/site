662b845d9db7da9782554fd7ee8dc6693e78bb12
ryzeiron <151409242+ryzeiron@users.noreply.github.com>
2026-05-11 14:16:31 +0200

    Update route.ts

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -140,7 +140,7 @@ export async function POST(request: Request) {
       mondialRelayExpeditionNumber,
       mondialRelayLabelUrl,
       mondialRelayError,
-      status: metadata.relay_code ? "label_to_create" : "paid",
+      status: "paid",
     })
     .onConflictDoNothing();
 
