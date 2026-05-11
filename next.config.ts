386cd16bbdda53f33d432ef72d6f34eefcd85e5b
ryzeiron <151409242+ryzeiron@users.noreply.github.com>
2026-05-11 11:26:03 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
+  outputFileTracingRoot: path.resolve(process.cwd()),
+
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "images.pokemontcg.io" },
