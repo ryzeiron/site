75ac059b851fce278563f501ea54d807eb7761ff
ryzeiron <151409242+ryzeiron@users.noreply.github.com>
2026-05-13 15:01:37 +0200

    Update NavMenu.tsx

--- components/NavMenu.tsx
+++ components/NavMenu.tsx
@@ -5,6 +5,7 @@ import { useEffect, useRef, useState } from "react";
 
 const LINKS = [
   { href: "/", label: "Accueil" },
+  { href: "/recherche", label: "Recherche" },
   { href: "/blocs", label: "Blocs" },
   { href: "/favoris", label: "Mes favoris" },
   { href: "/compte", label: "Mon compte" },
