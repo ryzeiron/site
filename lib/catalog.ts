8b6f7d9b14a116c5699a108dea95bd7046a1b5ce
ryzeiron <151409242+ryzeiron@users.noreply.github.com>
2026-05-14 18:05:10 +0200

    Fix import path for DIAMANT_ET_PERLE_CARDS

--- lib/catalog.ts
+++ lib/catalog.ts
@@ -7,7 +7,7 @@ import { NOIR_ET_BLANC_CARDS } from "./catalog/cards/noir-et-blanc";
 import { HEARTGOLD_SOULSILVER_CARDS } from "./catalog/cards/heartgold-soulsilver";
 import { PLATINE_CARDS } from "./catalog/cards/platine"; 
 import { ADL_CARDS } from "./catalog/cards/adl"; 
-import { DIAMANT_ET_PERLE_CARDS } from "./catalog/cards/adl";
+import { DIAMANT_ET_PERLE_CARDS } from "./catalog/cards/diamant-et-perle";
 // Catalogue : blocs > series > cartes 
 // Prix en euros (ex: 15 = 15 EUR, 0.5 = 50 centimes)
 
