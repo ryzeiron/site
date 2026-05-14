ba88464ce7ffae48bad305f05317f19ef84b7018
ryzeiron <151409242+ryzeiron@users.noreply.github.com>
2026-05-14 17:22:39 +0200

    Update catalog.ts

--- lib/catalog.ts
+++ lib/catalog.ts
@@ -6,7 +6,7 @@ import { XY_CARDS } from "./catalog/cards/xy";
 import { NOIR_ET_BLANC_CARDS } from "./catalog/cards/noir-et-blanc";
 import { HEARTGOLD_SOULSILVER_CARDS } from "./catalog/cards/heartgold-soulsilver";
 import { PLATINE_CARDS } from "./catalog/cards/platine"; 
-
+import { ADL_CARDS } from "./catalog/cards/adl"; 
 // Catalogue : blocs > series > cartes 
 // Prix en euros (ex: 15 = 15 EUR, 0.5 = 50 centimes)
 
@@ -375,6 +375,7 @@ export const CARDS: Card[] = [
   ...NOIR_ET_BLANC_CARDS,
   ...HEARTGOLD_SOULSILVER_CARDS,
   ...PLATINE_CARDS,
+  ...ADL_CARDS,
 ];
 
 // Helpers
