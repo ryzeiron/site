3f9100c8406c913969d3d3751f86fc8744269b70
ryzeiron <151409242+ryzeiron@users.noreply.github.com>
2026-05-28 00:22:25 +0200

    Update mcdo.ts

--- lib/catalog/cards/mcdo.ts
+++ lib/catalog/cards/mcdo.ts
@@ -4,16 +4,16 @@ import type { Card } from "../../catalog";
 export const MCDO_CARDS = [
 
   //MCDO 2011
-  { id: "2011bw-01", serieId: "2011bw", name: "Vipélierre ", number: "01/12", rarity: "commune", condition: "Near Mint", language: "FR", price: 0.5, stock: 0, image: "/cartes/mcdo/1.webp", },
-  { id: "2011bw-02", serieId: "2011bw", name: "Maracachi ", number: "02/12", rarity: "commune", condition: "Near Mint", language: "FR", price: 0.5, stock: 0, image: "/cartes/mcdo/2.webp", },
-  { id: "2011bw-03", serieId: "2011bw", name: "Gruikui ", number: "03/12", rarity: "commune", condition: "Near Mint", language: "FR", price: 0.5, stock: 0, image: "/cartes/mcdo/3.webp", },
-  { id: "2011bw-04", serieId: "2011bw", name: "Moustillon ", number: "04/12", rarity: "commune", condition: "Near Mint", language: "FR", price: 0.5, stock: 0, image: "/cartes/mcdo/4.webp", },
-  { id: "2011bw-05", serieId: "2011bw", name: "Mamanbo ", number: "05/12", rarity: "commune", condition: "Near Mint", language: "FR", price: 0.5, stock: 0, image: "/cartes/mcdo/5.webp", },
-  { id: "2011bw-06", serieId: "2011bw", name: "Zébibron ", number: "06/12", rarity: "commune", condition: "Near Mint", language: "FR", price: 0.5, stock: 0, image: "/cartes/mcdo/6.webp", },
-  { id: "2011bw-07", serieId: "2011bw", name: "Munna ", number: "07/12", rarity: "commune", condition: "Near Mint", language: "FR", price: 0.5, stock: 0, image: "/cartes/mcdo/7.webp", },
-  { id: "2011bw-08", serieId: "2011bw", name: "Mascaïman ", number: "08/12", rarity: "commune", condition: "Near Mint", language: "FR", price: 0.5, stock: 0, image: "/cartes/mcdo/8.webp", },
-  { id: "2011bw-09", serieId: "2011bw", name: "Zorua ", number: "09/12", rarity: "commune", condition: "Near Mint", language: "FR", price: 0.5, stock: 0, image: "/cartes/mcdo/9.webp", },
-  { id: "2011bw-10", serieId: "2011bw", name: "Tic ", number: "10/12", rarity: "commune", condition: "Near Mint", language: "FR", price: 0.5, stock: 0, image: "/cartes/mcdo/10.webp", },
-  { id: "2011bw-11", serieId: "2011bw", name: "Poichigeon ", number: "11/12", rarity: "commune", condition: "Near Mint", language: "FR", price: 0.5, stock: 0, image: "/cartes/mcdo/11.webp", },
-  { id: "2011bw-12", serieId: "2011bw", name: "Nanméouïe ", number: "12/12", rarity: "commune", condition: "Near Mint", language: "FR", price: 0.5, stock: 0, image: "/cartes/mcdo/12.webp", },
-] satisfies Card[];
+  { id: "2011bw-01", serieId: "2011bw", name: "Vipélierre ", number: "01/12", rarity: "commune", condition: "Near Mint", language: "FR", price: 0.5, stock: 0, image: "/cartes/2011/1.webp", },
+  { id: "2011bw-02", serieId: "2011bw", name: "Maracachi ", number: "02/12", rarity: "commune", condition: "Near Mint", language: "FR", price: 0.5, stock: 0, image: "/cartes/2011/2.webp", },
+  { id: "2011bw-03", serieId: "2011bw", name: "Gruikui ", number: "03/12", rarity: "commune", condition: "Near Mint", language: "FR", price: 0.5, stock: 0, image: "/cartes/2011/3.webp", },
+  { id: "2011bw-04", serieId: "2011bw", name: "Moustillon ", number: "04/12", rarity: "commune", condition: "Near Mint", language: "FR", price: 0.5, stock: 0, image: "/cartes/2011/4.webp", },
+  { id: "2011bw-05", serieId: "2011bw", name: "Mamanbo ", number: "05/12", rarity: "commune", condition: "Near Mint", language: "FR", price: 0.5, stock: 0, image: "/cartes/2011/5.webp", },
+  { id: "2011bw-06", serieId: "2011bw", name: "Zébibron ", number: "06/12", rarity: "commune", condition: "Near Mint", language: "FR", price: 0.5, stock: 0, image: "/cartes/2011/6.webp", },
+  { id: "2011bw-07", serieId: "2011bw", name: "Munna ", number: "07/12", rarity: "commune", condition: "Near Mint", language: "FR", price: 0.5, stock: 0, image: "/cartes/2011/7.webp", },
+  { id: "2011bw-08", serieId: "2011bw", name: "Mascaïman ", number: "08/12", rarity: "commune", condition: "Near Mint", language: "FR", price: 0.5, stock: 0, image: "/cartes/2011/8.webp", },
+  { id: "2011bw-09", serieId: "2011bw", name: "Zorua ", number: "09/12", rarity: "commune", condition: "Near Mint", language: "FR", price: 0.5, stock: 0, image: "/cartes/2011/9.webp", },
+  { id: "2011bw-10", serieId: "2011bw", name: "Tic ", number: "10/12", rarity: "commune", condition: "Near Mint", language: "FR", price: 0.5, stock: 0, image: "/cartes/2011/10.webp", },
+  { id: "2011bw-11", serieId: "2011bw", name: "Poichigeon ", number: "11/12", rarity: "commune", condition: "Near Mint", language: "FR", price: 0.5, stock: 0, image: "/cartes/2011/11.webp", },
+  { id: "2011bw-12", serieId: "2011bw", name: "Nanméouïe ", number: "12/12", rarity: "commune", condition: "Near Mint", language: "FR", price: 0.5, stock: 0, image: "/cartes/2011/12.webp", },
+] as unknown as Card[]);
