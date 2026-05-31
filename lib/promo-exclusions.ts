9993deba18dd31b554558cd9b2f07cf68b6e6478
ryzeiron <151409242+ryzeiron@users.noreply.github.com>
2026-05-31 16:25:10 +0200

    Delete promo-exclusions.ts

--- lib/promo-exclusions.ts
+++
@@ -1,7 +0,0 @@
-//export const PROMO_EXCLUDED_CARD_IDS = new Set([
- 
-//]);
-
-//export function isPromoExcludedCard(cardId: string): boolean {
-  //return PROMO_EXCLUDED_CARD_IDS.has(cardId);
-//}
